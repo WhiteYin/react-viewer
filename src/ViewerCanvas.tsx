09bc35d2f89ec4cb1b28ace71acf863179c630c7
whiteyin <yinoo0@163.com>
2024-06-24 21:01:42 +0800

    feat: 新增左右箭头切换前/后一张

--- src/ViewerCanvas.tsx
+++ src/ViewerCanvas.tsx
@@ -1,104 +1,107 @@
-import * as React from 'react';
-import Loading from './Loading';
-import classnames from 'classnames';
+import * as React from 'react'
+import Loading from './Loading'
+import classnames from 'classnames'
+import { ActionType } from './Icon'
 
 export interface ViewerCanvasProps {
-  prefixCls: string;
-  imgSrc: string;
-  visible: boolean;
-  width: number;
-  height: number;
-  top: number;
-  left: number;
-  rotate: number;
-  onChangeImgState: (width: number, height: number, top: number, left: number) => void;
-  onResize: () => void;
-  zIndex: number;
-  scaleX: number;
-  scaleY: number;
-  loading: boolean;
-  drag: boolean;
-  container: HTMLElement;
-  onCanvasMouseDown: (e: React.MouseEvent<HTMLDivElement>) => void;
+  prefixCls: string
+  imgSrc: string
+  visible: boolean
+  width: number
+  height: number
+  top: number
+  left: number
+  rotate: number
+  onChangeImgState: (width: number, height: number, top: number, left: number) => void
+  onResize: () => void
+  zIndex: number
+  scaleX: number
+  scaleY: number
+  loading: boolean
+  drag: boolean
+  container: HTMLElement
+  onCanvasMouseDown: (e: React.MouseEvent<HTMLDivElement>) => void
+  handleDefaultAction(type: ActionType): void
 }
 
 export interface ViewerCanvasState {
-  isMouseDown?: boolean;
-  mouseX?: number;
-  mouseY?: number;
+  isMouseDown?: boolean
+  mouseX?: number
+  mouseY?: number
 }
 
 export default function ViewerCanvas(props: ViewerCanvasProps) {
-  const isMouseDown = React.useRef(false);
+  const { handleDefaultAction } = props
+  const isMouseDown = React.useRef(false)
   const prePosition = React.useRef({
     x: 0,
     y: 0,
-  });
-  const [ position, setPosition ] = React.useState({
+  })
+  const [position, setPosition] = React.useState({
     x: 0,
     y: 0,
-  });
+  })
 
   React.useEffect(() => {
     return () => {
-      bindEvent(true);
-      bindWindowResizeEvent(true);
-    };
-  }, []);
+      bindEvent(true)
+      bindWindowResizeEvent(true)
+    }
+  }, [])
 
   React.useEffect(() => {
-    bindWindowResizeEvent();
+    bindWindowResizeEvent()
 
     return () => {
-      bindWindowResizeEvent(true);
-    };
-  });
+      bindWindowResizeEvent(true)
+    }
+  })
 
   React.useEffect(() => {
     if (props.visible && props.drag) {
-      bindEvent();
+      bindEvent()
     }
     if (!props.visible && props.drag) {
-      handleMouseUp({});
+      handleMouseUp({})
     }
     return () => {
-      bindEvent(true);
-    };
-  }, [props.drag, props.visible]);
+      bindEvent(true)
+    }
+  }, [props.drag, props.visible])
 
   React.useEffect(() => {
-    let diffX = position.x - prePosition.current.x;
-    let diffY = position.y - prePosition.current.y;
+    let diffX = position.x - prePosition.current.x
+    let diffY = position.y - prePosition.current.y
     prePosition.current = {
       x: position.x,
       y: position.y,
-    };
-    props.onChangeImgState(props.width, props.height, props.top + diffY, props.left + diffX);
-  }, [position]);
+    }
+    props.onChangeImgState(props.width, props.height, props.top + diffY, props.left + diffX)
+  }, [position])
 
   function handleResize(e) {
-    props.onResize();
+    props.onResize()
   }
 
   function handleCanvasMouseDown(e) {
-    props.onCanvasMouseDown(e);
-    handleMouseDown(e);
+    props.onCanvasMouseDown(e)
+    handleMouseDown(e)
   }
 
   function handleMouseDown(e) {
     if (e.button !== 0) {
-      return;
+      return
     }
     if (!props.visible || !props.drag) {
-      return;
+      return
     }
-    e.preventDefault();
-    e.stopPropagation();
-    isMouseDown.current = true;
+    e.preventDefault()
+    e.stopPropagation()
+    isMouseDown.current = true
     prePosition.current = {
       x: e.nativeEvent.clientX,
       y: e.nativeEvent.clientY,
-    };
+    }
   }
 
   const handleMouseMove = (e) => {
@@ -106,30 +109,30 @@ export default function ViewerCanvas(props: ViewerCanvasProps) {
       setPosition({
         x: e.clientX,
         y: e.clientY,
-      });
+      })
     }
-  };
+  }
 
   function handleMouseUp(e) {
-    isMouseDown.current = false;
+    isMouseDown.current = false
   }
 
   function bindWindowResizeEvent(remove?: boolean) {
-    let funcName = 'addEventListener';
+    let funcName = 'addEventListener'
     if (remove) {
-      funcName = 'removeEventListener';
+      funcName = 'removeEventListener'
     }
-    window[funcName]('resize', handleResize, false);
+    window[funcName]('resize', handleResize, false)
   }
 
   function bindEvent(remove?: boolean) {
-    let funcName = 'addEventListener';
+    let funcName = 'addEventListener'
     if (remove) {
-      funcName = 'removeEventListener';
+      funcName = 'removeEventListener'
     }
 
-    document[funcName]('click', handleMouseUp, false);
-    document[funcName]('mousemove', handleMouseMove, false);
+    document[funcName]('click', handleMouseUp, false)
+    document[funcName]('mousemove', handleMouseMove, false)
   }
 
   let imgStyle: React.CSSProperties = {
@@ -138,25 +141,20 @@ export default function ViewerCanvas(props: ViewerCanvasProps) {
     transform: `
 translateX(${props.left !== null ? props.left + 'px' : 'aoto'}) translateY(${props.top}px)
     rotate(${props.rotate}deg) scaleX(${props.scaleX}) scaleY(${props.scaleY})`,
-  };
+  }
 
   const imgClass = classnames(`${props.prefixCls}-image`, {
     drag: props.drag,
     [`${props.prefixCls}-image-transition`]: !isMouseDown.current,
-  });
+  })
 
   let style = {
     zIndex: props.zIndex,
-  };
+  }
 
-  let imgNode = null;
+  let imgNode = null
   if (props.imgSrc !== '') {
-    imgNode = <img
-    className={imgClass}
-    src={props.imgSrc}
-    style={imgStyle}
-    onMouseDown={handleMouseDown}
-    />;
+    imgNode = <img className={imgClass} src={props.imgSrc} style={imgStyle} onMouseDown={handleMouseDown} />
   }
   if (props.loading) {
     imgNode = (
@@ -168,18 +166,30 @@ translateX(${props.left !== null ? props.left + 'px' : 'aoto'}) translateY(${pro
           alignItems: 'center',
         }}
       >
-        <Loading/>
+        <Loading />
       </div>
-    );
+    )
   }
 
   return (
-    <div
-    className={`${props.prefixCls}-canvas`}
-    onMouseDown={handleCanvasMouseDown}
-    style={style}
-    >
+    <div className={`${props.prefixCls}-canvas`} onMouseDown={handleCanvasMouseDown} style={style}>
+      <div
+        className={`${props.prefixCls}-canvas-action prev`}
+        onClick={() => {
+          handleDefaultAction(ActionType.prev)
+        }}
+      >
+        &lt;
+      </div>
       {imgNode}
+      <div
+        className={`${props.prefixCls}-canvas-action next`}
+        onClick={() => {
+          handleDefaultAction(ActionType.next)
+        }}
+      >
+        &gt;
+      </div>
     </div>
-  );
+  )
 }
